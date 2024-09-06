652833a9ea992691475983a3315516b472681b0c
BARATH D <143203938+Barath-ash@users.noreply.github.com>
2024-09-06 09:46:34 +0530

    Update Todoitem.jsx
    Delete button size changed

--- src/Components/Todoitem.jsx
+++ src/Components/Todoitem.jsx
@@ -27,7 +27,7 @@ export default function Todoitem(props){
         <div className="flex justify-between ">
         <p className="">{props.index+1}.{props.item.activity}</p>
         <button onClick={() => {handledelte(props.item.id)}}>  
-        <TrashIcon className="size-6 text-red-500" />
+        <TrashIcon className="size-4 text-red-500" />
         </button>
         </div>
         </>
